b9024e15b430545264c2423466578fe5cfe0c3ed
inog <kontakt@ingo-reschke.de>
2019-09-27 15:21:32 +0200

    optimize Person. add status

--- src/main/java/katalist/Person.java
+++ src/main/java/katalist/Person.java
@@ -4,34 +4,27 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
+    private String status;
 
-    public Person(String firstName, String lastName, int age) {
+
+    public Person(String firstName, String lastName, int age, String status) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.status = status;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    public String getStatus() {return status; }
 }
