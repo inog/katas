16e9eec9ac6ecf2432065989f1af472d3341dbd1
inog <kontakt@ingo-reschke.de>
2019-09-29 16:25:15 +0200

    cleanup

--- src/main/java/katalist/Action.java
+++ src/main/java/katalist/Action.java
@@ -6,10 +6,6 @@ import java.util.stream.Collectors;
 public class Action {
 
     public List<Person> filterListByProperty(List<Person> personList){
-      //  List<Person> muellers = personList.stream()
-      //          .filter(person -> person.getLastName().equals("Müller"))
-      //          .collect(Collectors.toList());
-
         Map<Integer,Person> lastStatusMap = new LinkedHashMap<>();
         personList.forEach(person -> {
             lastStatusMap.put(person.getId(), person);
