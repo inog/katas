048a38799a80f5d6509716fa7e404d70694df3d3
Ingo Reschke <kontakt@ingo-reschke.de>
2019-12-19 00:09:56 +0100

    correct test
    Signed-off-by: inog <kontakt@ingo-reschke.de>

--- src/test/java/playwithstreams/CountTest.java
+++ src/test/java/playwithstreams/CountTest.java
@@ -1,18 +1,17 @@
 package playwithstreams;
 
 import org.assertj.core.api.Assertions;
-
 import org.junit.Test;
 
 import java.util.Arrays;
-import static org.junit.Assert.*;
+
 
 public class CountTest {
     Count cut = new Count();
 
     @Test
     public void count() {
-        long result = cut.count(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        long result = cut.count(Arrays.asList(1,2,3,4,5,6,7,8));
         Assertions.assertThat(result).isEqualTo(8);
     }
 }
