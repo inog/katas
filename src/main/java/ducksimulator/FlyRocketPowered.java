5cb69911096b5412c44b0f1f6ade69dead6579a1
inog <kontakt@ingo-reschke.de>
2020-01-30 10:49:13 +0100

    type the code example of the strategy pattern, from the Head First Design Patterns.
    Signed-off-by: inog <kontakt@ingo-reschke.de>

---
+++ src/main/java/ducksimulator/FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package ducksimulator;
+
+public class FlyRocketPowered implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("I'm flying with a rocket");
+    }
+}
---
+++ src/main/java/ducksimulator/Sqeak.java
@@ -0,0 +1,8 @@
+package ducksimulator;
+
+public class Sqeak implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Sqeak");
+    }
+}
