e01e492eecbc8b32b2a273083b1e6da65ab92c60
inog <kontakt@ingo-reschke.de>
2021-02-08 08:04:40 +0100

    format
    Signed-off-by: inog <kontakt@ingo-reschke.de>

--- src/main/java/codewars/DiffOf2Collections.java
+++ src/main/java/codewars/DiffOf2Collections.java
@@ -7,17 +7,13 @@ import java.util.stream.Collectors;
 
 public class DiffOf2Collections {
     public static List<Character> diff(Collection<Character> a, Collection<Character> b) {
-
         Set<Character> setA = a.stream()
                 .filter(item -> !b.contains(item))
                 .collect(Collectors.toSet());
         Set<Character> setB = b.stream()
                 .filter(item -> !a.contains(item))
                 .collect(Collectors.toSet());
-
         setA.addAll(setB);
-
-
         return  setA.stream().sorted().collect(Collectors.toList());
     }
 }
