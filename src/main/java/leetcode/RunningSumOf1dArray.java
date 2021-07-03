659f206afc929f9c6aeec4fc4a7345daf11004b0
inog <kontakt@ingo-reschke.de>
2021-07-03 16:31:00 +0200

    Leetcode init RuningSumOf1dArray
    Signed-off-by: inog <kontakt@ingo-reschke.de>

---
+++ src/main/java/leetcode/RunningSumOf1dArray.java
@@ -0,0 +1,21 @@
+package leetcode;
+/**
+ * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ *
+ * Return the running sum of nums.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,4]
+ * Output: [1,3,6,10]
+ * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+ *
+ * Constraints:
+ *
+ *     1 <= nums.length <= 1000
+ *     -10^6 <= nums[i] <= 10^6
+ */
+
+class RunningSumOf1dArray {
+
+
+}
---
+++ src/test/java/leetcode/RunningSumOf1dArrayTest.java
@@ -0,0 +1,5 @@
+package leetcode;
+
+class RunningSumOf1dArrayTest {
+
+}
