685526d49048dece0742b965b7c6532a906b9173
Ingo Reschke <kontakt@ingo-reschke.de>
2021-09-12 23:52:07 +0200

    add stream function and explore the kotlin language. improve test.
    There is still 1 assert which fails. :)
    
    Signed-off-by: Ingo Reschke <kontakt@ingo-reschke.de>

--- src/main/kotlin/codewarskt/DiffOf2Collections.kt
+++ src/main/kotlin/codewarskt/DiffOf2Collections.kt
@@ -1,10 +1,12 @@
-package codewars.diffof2colections.kotlin
+package codewarskt
 
-class DiffOf2Collections{
 
-    fun diff() : List<Character>{
-        return listOf()
+class DiffOf2Collections {
+
+    fun diff(listA: Collection<Char>, listB: Collection<Char>): List<Char> {
+        val setA = listA.toSet().filter { !listB.contains(it) }
+        val setB = listB.toSet().filter { !listA.contains(it) }
+        val resultSet = setA + setB
+        return resultSet.toList()
     }
-
-
 }
--- src/test/kotlin/codewarskt/DiffOf2CollectionsTest.kt
+++ src/test/kotlin/codewarskt/DiffOf2CollectionsTest.kt
@@ -1,6 +1,5 @@
 package codewarskt
 
-import codewars.diffof2colections.kotlin.DiffOf2Collections
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -10,9 +9,16 @@ class DiffOf2CollectionsTest {
     @Test
     fun testDiff(){
         val cut: DiffOf2Collections = DiffOf2Collections()
-        val result = cut.diff()
+        val result = cut.diff(emptyList(), emptyList())
         assertEquals(0, result.size, "should be 0")
-        assertTrue(result.isEmpty())
-    }
+        assertTrue(result.isEmpty(),"should return empty for the empty content")
 
+        assertEquals(listOf<Character>(), cut.diff(listOf<Char>('a', 'b'), listOf<Char>('a', 'b')), "should return empty for the same content")
+        assertEquals(listOf('a', 'b'), cut.diff(listOf('a', 'b'), emptyList()), "should return A if B is empty")
+        assertEquals(listOf('c', 'd'), cut.diff(emptyList(), listOf('c', 'd')), "should return B if A is empty")
+        assertEquals(listOf('z'), cut.diff(listOf('a', 'b', 'z'), listOf('a', 'b')), "should return the last character")
+        assertEquals(listOf('d', 'e', 'j', 'z'), cut.diff(listOf('a', 'b', 'z', 'd', 'e', 'd'), listOf('a', 'b', 'j', 'j')), "should return the sorted characters")
+
+
+    }
 }
